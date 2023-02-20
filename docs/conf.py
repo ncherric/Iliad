4f9d1c6d58bc13554be16215ec16cf1e43436f8d
Noah Herrick <ncherric@iupui.edu>
2023-02-20 11:05:24 -0500

    Read the Docs

--- docs/conf.py
+++ docs/conf.py
@@ -98,21 +98,21 @@ html_theme_options = {
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    'papersize': 'letterpaper',
-    # The font size ('10pt', '11pt' or '12pt').
-    'pointsize': '10pt',
-    # Additional stuff for the LaTeX preamble.
-    'preamble': '',
-}
+# latex_elements = {
+#     # The paper size ('letterpaper' or 'a4paper').
+#     'papersize': 'letterpaper',
+#     # The font size ('10pt', '11pt' or '12pt').
+#     'pointsize': '10pt',
+#     # Additional stuff for the LaTeX preamble.
+#     'preamble': '',
+# }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    ("index", "Iliad.tex", "Iliad Documentation", "Noah Herrick", "manual"),
-]
+# latex_documents = [
+#     ("index", "Iliad.tex", "Iliad Documentation", "Noah Herrick", "manual"),
+# ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
