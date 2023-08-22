41e5c9f9d2f6b328d94b857f8d949ef82711ce7d
Noah <ncherric@iupui.edu>
2023-08-22 09:24:35 -0400

    auto config script

---
+++ config/auto_config.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import yaml
+
+def main():
+
+    with open("config.yaml") as istream:
+        ymldoc = yaml.safe_load(istream)
+
+        # Prompt the user for a new value
+        WD = input("Enter your working directory: e.g. /Path/To/Directory/Iliad/")
+        ymldoc['workdirPath'] = WD
+
+        # Prompt the user for a new value
+        sampleFile = input("Enter your sample file location containing sample names and URLs (if you are downloading): copy if unchanged -> config/UserSampleTable.csv ")
+        ymldoc['samplesDict'] = sampleFile
+
+        # Prompt the user for a new value
+        refSpecies = input("Enter your reference assembly species: copy if unchanged -> homo_sapiens ")
+        ymldoc['ref']['species'] = refSpecies
+        refRelease = input("Enter your reference assembly release: copy if unchanged -> 104  ")
+        ymldoc['ref']['release'] = refRelease
+        refBuild = input("Enter your reference assembly build: copy if unchanged -> GRCh38  ")
+        ymldoc['ref']['build'] = refBuild
+
+    with open("modified.yaml", "w") as ostream:
+        yaml.dump(ymldoc, ostream, default_flow_style=False, sort_keys=False)
+
+if __name__ == "__main__":
+    main()
