0f9d717d0f6a0a7c0340c99f32e8fd6bde25c64c
Noah <ncherric@iupui.edu>
2023-08-22 09:28:26 -0400

    auto config script

--- config/auto_config.py
+++ config/auto_config.py
@@ -8,19 +8,19 @@ def main():
         ymldoc = yaml.safe_load(istream)
 
         # Prompt the user for a new value
-        WD = input("Enter your working directory: e.g. /Path/To/Directory/Iliad/")
+        WD = input("Enter your working directory. For example, something like this -> /Path/To/Directory/Iliad/: ")
         ymldoc['workdirPath'] = WD
 
         # Prompt the user for a new value
-        sampleFile = input("Enter your sample file location containing sample names and URLs (if you are downloading): copy if unchanged -> config/UserSampleTable.csv ")
+        sampleFile = input("Enter your sample file location containing sample names and URLs (if you are downloading). copy this if unchanged -> config/UserSampleTable.csv: ")
         ymldoc['samplesDict'] = sampleFile
 
         # Prompt the user for a new value
-        refSpecies = input("Enter your reference assembly species: copy if unchanged -> homo_sapiens ")
+        refSpecies = input("Enter your reference assembly species. copy this if unchanged -> homo_sapiens: ")
         ymldoc['ref']['species'] = refSpecies
-        refRelease = input("Enter your reference assembly release: copy if unchanged -> 104  ")
+        refRelease = input("Enter your reference assembly release. copy this if unchanged -> 104: ")
         ymldoc['ref']['release'] = refRelease
-        refBuild = input("Enter your reference assembly build: copy if unchanged -> GRCh38  ")
+        refBuild = input("Enter your reference assembly build. copy this if unchanged -> GRCh38: ")
         ymldoc['ref']['build'] = refBuild
 
     with open("modified.yaml", "w") as ostream:
